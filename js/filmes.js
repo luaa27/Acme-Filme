469c03de9bfe28a8b58babd4f334cfd60abae40a
LUANA DA SILVA SANTOS <23111813@CFP127ALUNO.LOCAL>
2024-04-04 17:20:14 -0300

    okay

---
+++ js/filmes.js
@@ -0,0 +1,69 @@
+export async function getFilmes(){
+    const url = 'http://localhost:8080/v2/FilmesACME/filmes'
+    const response = await fetch(url)
+    const data = await response.json()
+    
+
+    return data.filmes
+}
+
+export async function getFilmesId(id){
+    const url = `http://localhost:8080/v2/FilmesACME/filme/${id}`
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.filme[0]
+}
+
+export async function selectNameFilmes(nome){
+    const url = `http://localhost:8080/v2/FilmesACME/filmes/filtro?nomeFilme=${nome}`
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data);
+    return data
+}
+
+
+export async function postFilme(filme){
+    const url = 'http://localhost:8080/v2/acmeFilmes/filme'
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(filme)
+    }
+
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+export async function putFilme(id, filme){
+    const url = `http://localhost:8080/v2/acmeFilmes/filme/${id}`
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(filme)
+    }
+
+    const response = await fetch(url, options)
+
+    return response.ok
+}
+
+
+
+
+export async function deleteFilme(id){
+    try{
+        await fetch(`http://localhost:8080/v2/acmeFilmes/filme/${id}`,{
+            method: 'DELETE'
+        })
+        console.log("Filme excluído com sucesso")
+    } catch (error){
+        console.error('Erro ao excluir filme: ',error);
+    }
+}
+
